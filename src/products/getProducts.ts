cc71469befdae5a656a0d2abfd4755f9674335cf
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-29 21:26:56 +0100

    add types

--- src/products/getProducts.ts
+++ src/products/getProducts.ts
@@ -1,8 +1,11 @@
+import { Product } from "../Products";
+
 export async function getProducts() {
   console.log(window.location.origin + "/products");
   const response = await fetch(window.location.origin + "/products");
   if (!response.ok) {
     throw new Error("Failed to fetch products");
   }
-  return response.json();
+  const products: Product[] = await response.json();
+  return products;
 }
