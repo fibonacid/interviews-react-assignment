8ce96ed5721102ca76ab502cfca405296905372b
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-29 12:32:31 +0100

    format

--- src/Categories.tsx
+++ src/Categories.tsx
@@ -1,17 +1,32 @@
-import { Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 const drawerWidth = 180;
 
-const categories = ['Fruit', 'Vegetables', 'Dairy', 'Bakery', 'Meat', 'Seafood', 'Snacks', 'Beverages'];
+const categories = [
+  "Fruit",
+  "Vegetables",
+  "Dairy",
+  "Bakery",
+  "Meat",
+  "Seafood",
+  "Snacks",
+  "Beverages",
+];
 
 export const Categories = () => {
   return (
-    <Box minWidth={drawerWidth} sx={{ borderRight: '1px solid grey' }}>
+    <Box minWidth={drawerWidth} sx={{ borderRight: "1px solid grey" }}>
       <List>
         {categories.map((text) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
-              <ListItemText primary={text}/>
+              <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
