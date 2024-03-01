e32cf4e287d6d7a2d20f3fad320601806f349572
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-03-01 10:22:54 +0100

    fix error

--- src/products/useProducts.ts
+++ src/products/useProducts.ts
@@ -2,11 +2,11 @@ import { useEffect, useRef } from "react";
 import { getProducts } from "./getProducts";
 
 export function useProducts() {
-  const mountedRef = useRef(true);
+  const mountedRef = useRef(false);
 
   useEffect(() => {
-    if (!mountedRef.current) return;
-    mountedRef.current = false;
+    if (mountedRef.current) return;
+    mountedRef.current = true;
     getProducts();
   }, []);
 
