3bd7aeecf383805793541884ed886fa99a8d60e7
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-03-18 17:15:33 +0100

    remove over engineered code

--- src/products/ProductListing.tsx
+++
@@ -1,7 +0,0 @@
-import { ProductList } from "./ProductList";
-import { useProductsQuery } from "./useProductsQuery";
-
-export function ProductListing() {
-  const productsQuery = useProductsQuery();
-  return <ProductList products={productsQuery.data} />;
-}
